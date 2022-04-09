747865a2132d97f7eb44f2e62275cb626f830f4d
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2022-04-09 03:42:31 +0300

    Adding  document controller

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TemplateController;
+use App\Http\Controllers\DocumentController;
 
 
 /*
